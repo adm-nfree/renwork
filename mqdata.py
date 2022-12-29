a0640e893d5f4aa7615f3f2ae8ebd1e74c1cf0d7
n-free <narik-go@list.ru>
2022-12-29 13:45:18 +0530

    Filename: mqdata.py Version: 1.1

--- mqdata.py
+++ mqdata.py
@@ -18,37 +18,41 @@ def fetch(dirpath): # Fetching full information
     time.sleep(1)
     with open("mqlist", "r") as f:
         mqlist=f.read().split()
+        for i in mqlist:
+            result=re.findall(r'(?<=QMNAME\()(.*)(?=\))', i)
+            res=str(result[0])
+            os.popen("echo 'DISPLAY CHANNEL(*)' | runmqsc " + res + " | grep 'CHLTYPE' | grep -v SYSTEM > " + dirpath + "/" +"/" + res + ".info; echo 'DISPLAY LISTENER(*) PORT' | runmqsc " + res + " | grep LSTNR >> " + dirpath + "/" +"/" + res + ".info; echo 'DISPLAY QL(*)' | runmqsc " + res + " | grep QUEUE | grep -v SYSTEM >> " + dirpath + "/" +"/" + res + ".info")
 
-    for i in mqlist:
-        result=re.findall(r'(?<=QMNAME\()(.*)(?=\))', i)
-        res=str(result[0])
-        #print(res) #DEBUG
-        #print(type(res)) #DEBUG
-        os.popen("echo 'DISPLAY CHANNEL(*)' | runmqsc " + res + " | grep 'CHLTYPE' | grep -v SYSTEM > " + dirpath + "/" +"/" + res + ".info; echo 'DISPLAY LISTENER(*) PORT' | runmqsc " + res + " | grep LSTNR >> " + dirpath + "/" +"/" + res + ".info; echo 'DISPLAY QL(*)' | runmqsc " + res + " | grep QUEUE | grep -v SYSTEM >> " + dirpath + "/" +"/" + res + ".info")
+def nullcheck(dirpath): #Check mqlist for null
+    file=dirpath + "/mqlist"
+    filesize=os.path.getsize(file)
+    return filesize
+
 ######################################################################
 
 ### DEFINE VARIABLES #################################################
 dirpath = os.path.dirname(os.path.realpath(__file__)) # Full path to script
 
-print("""MQ data (C)N-Free|
--------------------
-Please, enter MQ manager name (full, or part), or enter "help" for more information
-""")
-arg = raw_input(">")
+print("MQ data (C)N-Free|\n-------------------\nPlease, enter MQ manager name (full, or part), or enter \"help\" for more information")
+arg = raw_input("-------------------\n>")
+print("-------------------")
 ######################################################################
 
 
-if arg == "help":
-    print("""
-You can enter full or part of MQ manager name example:
-Full: QMMCS.TEST11
-Part: MCS
-As result the programm will be create file(s) for each manager with full information.\n
-""")
-elif not arg:
-    print("argument is null")
+while arg == "help":
+    print("You can enter full or part of MQ manager name example:\nFull: QMMCS.TEST11\nPart: MCS\nAs result the programm will be create file(s) for each manager with full information.\n")
+    arg = raw_input("-------------------\n>")
+if not arg:
+    print("You didn't enter any argument.\nThe programm will provide information about all MQ managers. \nYou can find it in the script directory: " + dirpath)
+    time.sleep(1)
     without_arg(dirpath)
     fetch(dirpath)
 else:
+    print("Your argument is: " + arg + "\nThe programm will provide information about MQ managers, which contains the keyword.\nYou can find it in the script directory: " + dirpath)
     with_arg(arg, dirpath)
-    fetch(dirpath)
+    time.sleep(1)
+    filesize=nullcheck(dirpath)
+    if filesize == 0:
+        print("\nSorry, the programm didn't found any MQ Manager with your keyword. \nPlese make sure that you enter right name and try again.")
+    else:
+        fetch(dirpath)
